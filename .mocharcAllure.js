b5b89d46091f77b218efe191df211cd04cbc2db0
Reinaldo <reinaldo.rossetti@outlook.com>
2023-04-10 11:05:21 -0300

    adicionando testes em paralelo

--- .mocharcAllure.js
+++ .mocharcAllure.js
@@ -3,6 +3,8 @@
 module.exports = {
   reporter: "allure-mocha",
   require: ['test/baseTests/global.js'],
+  parallel: true,
+  jobs: 4,
   timeout: 5000,
   ui: "bdd",
   spec: ["test/features/openweathermap/*.test.js"]
