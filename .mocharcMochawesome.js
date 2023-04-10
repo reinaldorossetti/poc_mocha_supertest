5fe8ba49dc223f9abc88e41dd3a3ed5cd672660f
Reinaldo <reinaldo.rossetti@outlook.com>
2023-04-10 12:56:27 -0300

    ajustando a config

--- .mocharcMochawesome.js
+++ .mocharcMochawesome.js
@@ -3,10 +3,9 @@
 module.exports = {
   reporter: "mochawesome",
   parallel: true,
-  jobs: 5,
-  recursive: true,
-  reporterOptions: 'json=true,html=true,reportDir=report,reportFilename=index',
-  require: ['test/baseTests/global.js','mochawesome/register'],
+  jobs: 4,
+  reporterOptions: 'json=false,html=true,reportDir=report,reportFilename=index',
+  require: ['test/baseTests/global.js'],
   timeout: 5000,
   ui: "bdd",
   spec: ["test/features/openweathermap/*.test.js"]
